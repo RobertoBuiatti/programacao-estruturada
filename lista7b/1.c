26120b0c47036fda663e7ae54b23203b4b0ca10c
Roberto Caetano Buiatti <102183235+RobertoBuiatti@users.noreply.github.com>
2022-10-10 17:33:48 -0300

    lista 7

---
+++ lista7b/1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    srand(time(NULL));
+    int imagem[3][3][3];
+    int i,j,k;
+    printf("matriz 3x3x3: \n");
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            for(k=0;k<3;k++){
+                imagem[i][j][k] = rand() % 10;
+                printf("%2d ", imagem[i][j][k]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+    return 0;
+}
